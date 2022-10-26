5f44c4a8615eb0ae37fcb6c4b3dbbfc0dc24ee5b
rcardin <riccardo.cardin@gmail.com>
2022-10-26 10:59:05 +0200

    WIP Switch to GlobalScope in KActorContext. I've to understand if it's the right choice

--- src/main/kotlin/in/rcard/kactor/KActorContext.kt
+++ src/main/kotlin/in/rcard/kactor/KActorContext.kt
@@ -1,21 +1,19 @@
 package `in`.rcard.kactor
 
-import kotlinx.coroutines.async
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.Channel.Factory.UNLIMITED
-import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 class KActorContext<out T> {
     suspend fun <U> spawn(behavior: KBehavior<U>, name: String): KActorRef<U> {
         val mailbox = Channel<U>(UNLIMITED)
         val actor = KActor(name, KActorContext(), mailbox)
         println("Creating actor '$name'")
-        coroutineScope {
-            async {
-                actor.run(behavior)
-            }
+        GlobalScope.launch {
+            actor.run(behavior)
+            println("Actor '$name' created")
         }
-        println("Actor '$name' created")
         return ChannelKActorRef(mailbox)
     }
 }

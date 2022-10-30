c9f908113d2f216dee1d26d07ed03b9442acd82e
rcardin <riccardo.cardin@gmail.com>
2022-10-30 21:46:26 +0100

    WIP Still stuck. It seems to be no solution :(

--- src/main/kotlin/in/rcard/kactor/KActorMainV2.kt
+++ src/main/kotlin/in/rcard/kactor/KActorMainV2.kt
@@ -1,46 +1,69 @@
 package `in`.rcard.kactor
 
+import `in`.rcard.kactor.v2.KActorRef.KActorRefOps.`!`
+import `in`.rcard.kactor.v2.kactor
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ReceiveChannel
 import kotlinx.coroutines.channels.SendChannel
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 
 suspend fun main(): Unit = coroutineScope {
-    TODO()
+    val mainActorRef = kactor(v2.MainActor.behavior())
+    mainActorRef `!` v2.MainActor.Start
 }
 
 object v2 {
+    object HelloWorldActor {
+        data class SayHello(val name: String)
+
+        suspend fun behavior(): KBehavior<SayHello> = coroutineScope {
+            receive {
+                println("Hello ${it.name}!")
+                KBehaviors.same()
+            }
+        }
+    }
 
     object MainActor {
 
         object Start
 
-//        suspend fun behavior(): KBehavior<Start> = kactor {
-//            coroutineScope {
-//                for (i in 0..100) {
-//                    println("Spawning actor $i")
-//                    val actorRef = spawn(HelloWorldActor.behavior(), "HelloWorldActor_$i")
-//                    println("Spawned actor $i")
-//                    actorRef `!` HelloWorldActor.SayHello("Riccardo")
-//                }
-//                KBehaviors.same()
-//            }
-//        }
+        suspend fun behavior(): KBehavior<Start> = coroutineScope {
+            receive {
+                for (i in 0..100) {
+                    println("Spawning actor $i")
+                    val helloWorldActorRef = kactor(HelloWorldActor.behavior())
+                    helloWorldActorRef `!` HelloWorldActor.SayHello("Riccardo")
+                }
+                KBehaviors.same()
+            }
+        }
     }
 
     // TODO Maybe here we need something different, moving the behavior to the actor?
-    fun <T> CoroutineScope.receive(receivedBehaviour: suspend (msg: T) -> KBehavior<T>): KBehavior<T> {
-        TODO()
+    fun <T> receive(receivedBehaviour: suspend (msg: T) -> KBehavior<T>): KBehavior<T> {
+        return KExtensibleBehavior(receivedBehaviour)
     }
 
     fun <T> CoroutineScope.kactor(behavior: KBehavior<T>): KActorRef<T> {
         val mailbox = Channel<T>()
         launch {
-            val msg = mailbox.receive()
-            behavior.receive(msg)
+            val actor = KActor(mailbox)
+            actor.run(behavior)
         }
         return KActorRef(mailbox)
+    }
+
+    class KActor<T>(private val receiveChannel: ReceiveChannel<T>) {
+        suspend fun run(behavior: KBehavior<T>) {
+            val msg = receiveChannel.receive()
+            when (val newBehavior = behavior.receive(msg)) {
+                is KBehaviorSame -> run(behavior)
+                is KExtensibleBehavior -> run(newBehavior)
+            }
+        }
     }
 
     class KActorRef<T> internal constructor(private val mailbox: SendChannel<T>) {
@@ -55,9 +78,29 @@ object v2 {
         }
     }
 
-    class KBehavior<T>(private val receivedBehaviour: suspend (msg: T) -> KBehavior<T>) {
-        suspend fun receive(msg: T): KBehavior<T> {
+    interface KBehavior<in T> {
+        suspend fun receive(msg: T): KBehavior<T>
+    }
+
+    // FIXME: This should be KBehavior<Nothing>
+    object KBehaviorSame : KBehavior<Any> {
+        override suspend fun receive(msg: Any): KBehavior<Any> {
+            return this
+        }
+    }
+
+    class KExtensibleBehavior<in T>(private val receivedBehaviour: suspend (msg: T) -> KBehavior<T>) :
+        KBehavior<T> {
+        override suspend fun receive(msg: T): KBehavior<T> {
             return receivedBehaviour(msg)
         }
+    }
+
+    object KBehaviors {
+        fun <T> receive(msgToBehavior: suspend (msg: T) -> KBehavior<T>): KBehavior<T> {
+            return KExtensibleBehavior(msgToBehavior)
+        }
+
+        fun same(): KBehavior<Any> = KBehaviorSame
     }
 }

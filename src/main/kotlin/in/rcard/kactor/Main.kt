95f7dcb070678b0131e632d20419eee4d48d802f
rcardin <riccardo.cardin@gmail.com>
2022-11-01 21:32:59 +0100

    Small fixes

--- src/main/kotlin/in/rcard/kactor/Main.kt
+++ src/main/kotlin/in/rcard/kactor/Main.kt
@@ -1,9 +1,10 @@
 package `in`.rcard.kactor
 
-import `in`.rcard.kactor.examples.AskPattern.askPattern
 import kotlinx.coroutines.coroutineScope
 
 // FIXME Just for testing purposes. Delete it in the near future
 suspend fun main() = coroutineScope {
-    askPattern()
+//    spawningActor()
+//    requestResponsePattern()
+//    askPattern()
 }

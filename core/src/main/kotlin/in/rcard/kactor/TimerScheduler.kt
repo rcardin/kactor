0fa70f4fa3ef3efaa0f0a756865ef490074c72b3
rcardin <riccardo.cardin@gmail.com>
2022-12-06 13:55:08 +0100

    WIP First version of the TimerScheduler

--- core/src/main/kotlin/in/rcard/kactor/TimerScheduler.kt
+++ core/src/main/kotlin/in/rcard/kactor/TimerScheduler.kt
@@ -1,13 +1,32 @@
 package `in`.rcard.kactor
 
+import `in`.rcard.kactor.KActorRef.KActorRefOps.`!`
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.cancellable
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import kotlin.time.Duration
 
-class TimerScheduler {
-    fun <K, T> startSingleTimer(timerKey: K, msg: T, delay: Duration) {
-        TODO()
+class TimerScheduler<K, T>(private val scope: CoroutineScope, private val actorRef: KActorRef<T>) {
+
+    private val timers = mutableMapOf<K, Job>()
+    suspend fun startSingleTimer(timerKey: K, msg: T, delayTime: Duration) {
+        val flowJob = scope.launch {
+            flow {
+                while (true) {
+                    delay(delayTime)
+                    emit(msg)
+                }
+            }.onEach { actorRef `!` it }.cancellable().collect {}
+        }
+        timers[timerKey] = flowJob
     }
 
-    fun <K> cancel(timerKey: K) {
-        TODO()
+    fun cancel(timerKey: K) {
+        timers[timerKey]?.cancel("Timer with key $timerKey cancelled")
     }
 }

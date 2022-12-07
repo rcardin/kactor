7338b2beb199ee0c7f87c3c6ff8f76f5b0095d9b
rcardin <riccardo.cardin@gmail.com>
2022-12-07 09:43:28 +0100

    Used launchIn method in flow definition

--- core/src/main/kotlin/in/rcard/kactor/TimerScheduler.kt
+++ core/src/main/kotlin/in/rcard/kactor/TimerScheduler.kt
@@ -7,22 +7,20 @@ import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.cancellable
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.launch
 import kotlin.time.Duration
 
 class TimerScheduler<T>(private val scope: CoroutineScope, private val actorRef: KActorRef<T>) {
 
     private val timers = mutableMapOf<Any, Job>()
     suspend fun <K : Any> startSingleTimer(timerKey: K, msg: T, delayTime: Duration) {
-        val flowJob = scope.launch {
-            flow {
-                while (true) {
-                    delay(delayTime)
-                    emit(msg)
-                }
-            }.onEach { actorRef `!` it }.cancellable().collect {}
-        }
+        val flowJob = flow {
+            while (true) {
+                delay(delayTime)
+                emit(msg)
+            }
+        }.onEach { actorRef `!` it }.cancellable().launchIn(scope)
         timers[timerKey] = flowJob
     }
 

07aed7060957229b16c94482854faee2d456a363
rcardin <riccardo.cardin@gmail.com>
2023-04-15 11:12:52 +0200

    Added the dokka plugin to generate javadoc

--- core/src/main/kotlin/in/rcard/kactor/KActorRef.kt
+++ core/src/main/kotlin/in/rcard/kactor/KActorRef.kt
@@ -2,6 +2,11 @@ package `in`.rcard.kactor
 
 import kotlinx.coroutines.channels.SendChannel
 
+/**
+ * A reference to an actor.
+ *
+ * @param T The type of the messages that the actor can receive.
+ */
 class KActorRef<T> internal constructor(
     private val mailbox: SendChannel<T>
 ) {
